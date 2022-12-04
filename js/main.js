2eb849286a4602a7b0ed849a9a2f157679d97c38
Kyaw Sitt Thway <thwaykyawsitt@yahoo.com>
2022-12-04 11:13:32 +0630

    Added Model Popup menu to the home page

--- js/main.js
+++ js/main.js
@@ -6,3 +6,14 @@ let closeMenu = () => {
 let openMenu = () => {
     menu.style.right = "0";
 }
+
+// Popup menu OnLoad function
+let modalPopUp = () => {
+    var popup = document.getElementById("myPopup");
+    popup.style.display = "block";
+}
+
+let popupClose = () => {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+}
